45ab07c352be56e80baa112720bb535f04a12a58
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-08-07 00:47:18 +0300

    add task3

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int num = new Random().Next(1,20);
+Console.WriteLine(num);
+if (num % 2 == 0)
+{
+    System.Console.WriteLine("четное");
+}
+else
+{
+    System.Console.WriteLine("нечетное");
+}
